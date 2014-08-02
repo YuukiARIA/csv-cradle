dd810e9242dc1bf0f726da2bc6ce0eddd4832d77
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2014-08-03 00:58:34 +0900

    Add class CSV

---
+++ src/csvcradle/model/CSV.java
@@ -0,0 +1,28 @@
+package csvcradle.model;
+
+import java.util.List;
+
+public class CSV
+{
+	private List<Row> rows;
+
+	public CSV(List<Row> rows)
+	{
+		this.rows = rows;
+	}
+
+	public Row getRow(int i)
+	{
+		return rows.get(i);
+	}
+
+	public String[][] toStringTable()
+	{
+		String[][] rowsArray = new String[rows.size()][];
+		for (int i = 0; i < rowsArray.length; i++)
+		{
+			rowsArray[i] = rows.get(i).toStringArray();
+		}
+		return rowsArray;
+	}
+}
