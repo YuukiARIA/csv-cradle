a87d9e2e7eff224e54b9dfe32f3ca7980cba521e
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2014-08-03 18:26:36 +0900

    Implement Comparable interface on Location

--- src/csvcradle/model/parser/Location.java
+++ src/csvcradle/model/parser/Location.java
@@ -1,6 +1,6 @@
 package csvcradle.model.parser;
 
-public final class Location
+public final class Location implements Comparable<Location>
 {
 	public final int line;
 	public final int column;
@@ -9,6 +9,30 @@ public final class Location
 	{
 		this.line = line;
 		this.column = column;
+	}
+
+	public int hashCode()
+	{
+		return 17 * line + column;
+	}
+
+	public boolean equals(Object o)
+	{
+		return o == this || o instanceof Location
+			&& line == ((Location)o).line && column == ((Location)o).column;
+	}
+
+	public int compareTo(Location l)
+	{
+		if (line == l.line)
+		{
+			if (column == l.column)
+			{
+				return 0;
+			}
+			return column < l.column ? -1 : 1;
+		}
+		return line < l.line ? -1 : 1;
 	}
 
 	public String toString()
