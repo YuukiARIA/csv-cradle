f741d3cfa3a81cf6f50cbaed48c8dc3680b83d16
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2014-08-04 00:35:56 +0900

    Add build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+SRCDIR=${PWD}/src
+OUTDIR=${PWD}/bin
+PRODUCTDIR=${PWD}/product
+MAINCLASS=csvcradle.Main
+
+if [ -e ${OUTDIR} ]; then
+  rm -rf ${OUTDIR}
+fi
+
+mkdir ${OUTDIR}
+mkdir -p ${PRODUCTDIR}
+
+javac -d ${OUTDIR} \
+      -source 1.7 \
+      -target 1.7 \
+      -encoding UTF-8 \
+      `find ${SRCDIR} -name "*.java"`
+
+echo "Manifest-Version: 1.0" > manifest.mf
+echo "Main-Class: ${MAINCLASS}" >> manifest.mf
+
+jar cfm ${PRODUCTDIR}/csv-cradle.jar manifest.mf -C ${OUTDIR}/ .
+
+rm manifest.mf
+
+echo "#!/bin/sh" > ${PRODUCTDIR}/csv-cradle
+echo 'java -jar `dirname $0`/csv-cradle.jar $@' >> ${PRODUCTDIR}/csv-cradle
+chmod 755 ${PRODUCTDIR}/csv-cradle
